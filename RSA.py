d26659e7eec33029cf85c74ce9ebfed8b0e32237
JoakimSkoog97 <49760336+JoakimSkoog97@users.noreply.github.com>
2019-05-14 16:06:44 +0200

    Add files via upload

--- RSA.py
+++ RSA.py
@@ -25,8 +25,8 @@ def calculate_CTF(p, q):
 
 
 def is_prime(x): #Bad Alg
-	i = 0
-	while i < x/2:
+	i = 2
+	while i < x:
 		if x%i == 0:
 			return False
 		i += 1
@@ -35,7 +35,7 @@ def is_prime(x): #Bad Alg
 
 
 def calculate_e(CTF, p, q):
-	e = 3:
+	e = 3
 	have_found_e = False
 	while not have_found_e:
 		if is_prime(e):
@@ -46,8 +46,42 @@ def calculate_e(CTF, p, q):
 
 	return e
 				
+def calculate_d(CTF, e):
+	pass
 
+def miller_rabin(n):
+	nTmp = n
+	aList = [2, 3, 5, 7, 11, 13, 
+			17, 19, 23, 29, 31, 37, 
+			41, 43, 47, 53, 59, 61, 
+			67, 71, 73, 79, 83, 89, 
+			97, 101, 103, 107, 109, 
+			113, 127, 131, 137, 139, 
+			149, 151, 157, 163, 167, 
+			173, 179, 181, 191, 193, 
+			197, 199]
+	if nTmp % 2 ==0 or nTmp%3 ==0:
+		return False
 
+	else:
+		nTmp -= 1
+		s = 0
+		while nTmp %2 != 0:
+			s += 1
+			nTmp = nTmp /2
+
+		for a in aList:
+			if a < nTmp:
+				if (a**nTmp)%n == 1 and (a**nTmp)%n == 1 and (a**nTmp)%n == 1:
+					pass
+				else:
+					return False
+
+		return True
+
+for a in range(4,100000):
+	print(miller_rabin(a),":", is_prime(a), ":", a)
+	assert(miller_rabin(a) == is_prime(a))
 
 
 
